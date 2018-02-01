0b263bba050788bbaa61cb0f055d43c8fadbbc40
LynnAlan <191076550@qq.com>
2018-02-01 11:53:41 +0800

    修改整个项目架构

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# bird
-小鸟项目
-
+# 第一步 cnpm install 
+# 第二步 在当前目录 gulp dev 
+# 选中app/index.html 或再浏览器敲 http://localhost:8080/app/index.html
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,99 @@
-	
-	
-	//多个css操作：
-	var gulp = require('gulp');
-	gulp.task('watchFile', ['MiniCss', 'MiniJs'], function() {
-	    gulp.watch('js/*.js', ['MiniJs']);
-	    gulp.watch('css/*.css', ['MiniCss'])	
-	});
-	
-	
-  var concat = require('gulp-concat');  //合并
-  var autoprefixer = require('gulp-autoprefixer'); //处理前缀
-  var rename = require("gulp-rename");	   //重命名
-  var cleanCSS = require('gulp-clean-css');  //压缩css
-
-  var connect = require('gulp-connect');
-
-	gulp.task('csstask', function () {
-	    gulp.src('css/*.css')
-	        .pipe(autoprefixer({
-	            browsers: ['last 2 versions', 'Android >= 4.0',"> 5%"],
-	            cascade: true, //是否美化属性值 默认：true 像这样：
-	            //-webkit-transform: rotate(45deg);
-	            //        transform: rotate(45deg);
-	            remove:true //是否去掉不必要的前缀 默认：true 
-	        }))
-	        .pipe(cleanCSS())
-	        .pipe(concat('app.min.css'))
-	//	        .pipe( rename('app.min.css'))
-	        .pipe(gulp.dest('dist/css')) 
-	        .pipe(connect.reload());  /*css改变后压缩完成后 重新启动web服务*/
-	});  	
-	 // 用来设置开启服务器环境的任务
-	gulp.task('server', function() {
-	    connect.server({	    	
-	        port: 8000, // 更改端口号的  直接放上你要的端口号	       
-	        livereload: true
-	    });
-	});
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer');
+var webserver = require('gulp-webserver');
+var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var cleanCSS = require('gulp-clean-css');
 
 
-//监听文件改变的任务  只要文件改变就会重新调用压缩css的任务
-gulp.task('watchFile', ['csstask','server'], function() {
-
-	//检测到css文件的变化  执行压缩的任务
-    gulp.watch('css/*.css', ['csstask']); 
-    
-    //gulp.watch('js/*.js', ['jstask']); 
-
+var uglifyConfig = [
+    'static/js/jquery.min.js',
+    'static/js/jquery.cookie.js',
+]
+gulp.task('clean', function () {
+    return gulp.src('app/css/*')
+        .pipe(clean());
 });
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+gulp.task('concat', function () {
+    gulp.src(uglifyConfig)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('static/js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('static/js'))
+
+})
+
+gulp.task('concatjs', function () {
+    gulp.src('./app/js/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('static/js'))
+        // .pipe(uglify())
+        // .pipe(gulp.dest('static/js'))
+
+})
+
+
+gulp.task('concatcss',['clean','sass'],function(){
+    gulp.src('./app/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('app/css'))
+        // .pipe(uglify())
+        // .pipe(cleanCSS())
+        // .pipe(gulp.dest('static/css'))
+})
+
+gulp.task("sass", function () {
+    return gulp.src('app/sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+       
+})
+
+gulp.task('autoprefixer',['concatcss'], () =>
+    gulp.src('app/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('app/css'))
+);
+
+gulp.task('webserver',['concatcss'], function () {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            directoryListing: {
+                enable:true
+            },
+            port:8080,
+            // path:'./app'
+            // fallback: 'index.html'
+        }));
+});
+
+gulp.task('watch', function() { //这里的watch，是自定义的，携程live或者别的也行  
+    livereload.listen();//这里需要注意！旧版使用var server = livereload();已经失效    
+    gulp.watch('app/**/*.*', function(event) {  
+        livereload.changed(event.path);
+        gulp.watch('app/sass/*.scss',['sass','concatcss'])
+    }); 
+    gulp.watch('static/**/*.*', function(event) {  
+        livereload.changed(event.path);  
+    }); 
+
+});  
+
+
+
+gulp.task('dev', ['clean','sass', 'concat','autoprefixer','concatcss','webserver','watch']);
+
